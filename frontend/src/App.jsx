2642eb10dcf85296686ab5189e88fafb99be0bfd
hamzaKhan2004 <khanhamzatz@gmail.com>
2026-09-20 12:28:13 +0530

    set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="w-full h-screen bg-zinc-900 text-white">App</div>;
+};
+
+export default App;
---
+++ frontend/src/authContext.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-hooks/set-state-in-effect */
+import { createContext, useState, useEffect, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setCurrentUser(userId);
+    }
+  }, []);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
